d8723ddefbe22890b802ca0a19d0713c0b5631f7
Graeme Stuart <ggstuart@gmail.com>
2012-01-25 10:19:13 +0000

    bump to version 0.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name='pyjEplus',
-    version='0.0.3',
+    version='0.1',
     description='Python library for extracting data from jEplus output files',
     author='Graeme Stuart',
     author_email='ggstuart@gmail.com',
